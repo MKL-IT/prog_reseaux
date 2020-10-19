bda9fdc51692b19d6c7f969ae6719da09e1e7f28
MKL-IT <45044059+MKL-IT@users.noreply.github.com>
2020-10-18 22:43:58 -0700

    init commit

---
+++ TP1/UDP/MulticastClientThread.java
@@ -0,0 +1,62 @@
+/***
+ * ClientThread
+ * Example of a TCP server
+ * Date: 14/12/08
+ * Authors:
+ */
+
+package stream;
+
+import java.io.*;
+import java.net.*;
+
+public class MulticastClientThread extends Thread {
+	
+	private MulticastSocket multiSocket;
+  boolean go_on;
+	
+	MulticastClientThread (MulticastSocket ms) {
+		this.multiSocket = ms;
+    go_on = true;
+	}
+
+  /*public void interrupt() {
+      super.interrupt();
+      try {
+        multiSocket.close(); // Fermeture du flux si l'interruption n'a pas fonctionné.
+      } catch (SocketException se) {}
+  }*/
+
+ 	/**
+  	* receives a request from client then sends an echo to the client
+  	* @param clientSocket the client socket
+  	**/
+	public void run() {
+  	  try {
+    		
+        while (true) {
+
+    		  // Build a datagram packet for response
+          byte[] buf = new byte[1000];
+          DatagramPacket recv = new
+          DatagramPacket(buf, buf.length);
+
+          // Receive a datagram packet response
+          multiSocket.receive(recv);
+
+          // Print the response
+          System.out.println(new String(buf));
+          
+  		  }
+
+      } catch (SocketException se){
+          System.exit(1);
+
+    	} catch (Exception e) {
+        	System.err.println("Error in EchoServer:" + e); 
+      }
+  }
+  
+}
+
+  
